ca799b2c1bcef45b66c391a9e91c6c4b60c24980
dainv560 <nguyenvandai.8993@gmail.com>
2014-07-03 23:11:41 +0700

    Add TabHost content

---
+++ ShareApp/src/vn/vfossa/shareapp/ImageActivity.java
@@ -0,0 +1,14 @@
+package vn.vfossa.shareapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ImageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.images_activity);
+	}
+
+}
--- ShareApp/src/vn/vfossa/shareapp/MainActivity.java
+++ ShareApp/src/vn/vfossa/shareapp/MainActivity.java
@@ -1,15 +1,51 @@
 package vn.vfossa.shareapp;
 
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 
+	private TabHost tabHost;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        
+        tabHost = getTabHost();
+        
+        TabSpec appspec = tabHost.newTabSpec("UngDung");
+  		appspec.setIndicator("Ứng dụng");
+  		Intent appsIntent = new Intent(this, AppActivity.class);
+  		appspec.setContent(appsIntent);
+        
+  		TabSpec photospec = tabHost.newTabSpec("HinhAnh");
+  		photospec.setIndicator("Hình ảnh");
+  		Intent photosIntent = new Intent(this, ImageActivity.class);
+  		photospec.setContent(photosIntent);
+
+  		TabSpec songspec = tabHost.newTabSpec("NgheNhac");
+  		songspec.setIndicator("Nghe nhạc");
+  		Intent songsIntent = new Intent(this, MusicActivity.class);
+  		songspec.setContent(songsIntent);
+
+  		TabSpec videospec = tabHost.newTabSpec("XemPhim");
+  		videospec.setIndicator("Xem phim");
+  		Intent videosIntent = new Intent(this, VideoActivity.class);
+  		videospec.setContent(videosIntent);
+
+  		tabHost.addTab(appspec);
+  		tabHost.addTab(photospec);
+  		tabHost.addTab(songspec);
+  		tabHost.addTab(videospec);
     }
 
 
