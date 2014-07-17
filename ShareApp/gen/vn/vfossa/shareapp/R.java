d221897e26a6800b2cb3c161ccd8836a77fb84de
dainv560 <nguyenvandai.8993@gmail.com>
2014-07-18 00:48:34 +0700

    change layout and some function

--- ShareApp/gen/vn/vfossa/shareapp/R.java
+++ ShareApp/gen/vn/vfossa/shareapp/R.java
@@ -25,13 +25,15 @@ public final class R {
         public static final int music=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int appList=0x7f080004;
-        public static final int appListLayout=0x7f080005;
+        public static final int ScrollView01=0x7f080006;
+        public static final int action_settings=0x7f080008;
         public static final int autoCompleteTextView1=0x7f080000;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080003;
+        public static final int btScan=0x7f080002;
+        public static final int button2=0x7f080004;
+        public static final int button3=0x7f080005;
+        public static final int fileList=0x7f080007;
+        public static final int horizontalScrollView1=0x7f080003;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -46,7 +48,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int device=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int progress=0x7f050005;
+        public static final int scan=0x7f050007;
+        public static final int search=0x7f050006;
+        public static final int share=0x7f050004;
     }
     public static final class style {
         /** 
